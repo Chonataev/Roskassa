52d3206997e9b9b82456e6ef4586545f90159fd3
Chontaev <65342023+Chontaev@users.noreply.github.com>
2021-03-06 19:11:19 +0300

    Create rkassa.php

---
+++ config/rkassa.php
@@ -0,0 +1,8 @@
+<?php
+
+return[
+    'DOMAIN'     =>  env('DOMAIN','pay.roskassa.net'),
+    'SHOP_ID'    =>  env('SHOP_ID','9CFF252E3074FC9682187D5165A6DF33'),
+    'SECRET_KEY' =>  env('SECRET_KEY','Ild5m1Jf'),
+    'locale'     =>  "ru",  // ru || en
+];
